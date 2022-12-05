804445f25922769c9a0a863f087aeb9ded489a99
Colisian McLeod <mcleodom16@gmail.com>
2022-12-04 22:49:55 -0500

    Chatroom Server

--- Chatroom.java
+++ Chatroom.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.ServerSocket;
 /**
  * Chatroom
  */
@@ -5,11 +9,44 @@
 
 public class Chatroom {
 
-    public Chatroom(){
+    private ServerSocket serverSocket;
+
+    public Chatroom(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
 
     }
 
-    public static void main(String[] args) {
+    public void startServer(){
+
+        try{
+            while (!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("New User added!");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+            }
+        } catch (IOException e) {
+
+        }
+         public void closeServerSocket(){
+            try{
+                if (serverSocket!= null){
+                    serverSocket.close();
+                }
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Chatroom server = new Chatroom(serverSocket);
+        server.startServer();
         System.out.println("Testing");
         System.out.println("Testing 2");
     }
